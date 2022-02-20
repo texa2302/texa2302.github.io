2c095311c82f6dfb3604da8c256747f8ad49bd81
texa2302 <fedya2302@mail.ru>
2022-02-20 16:53:08 +0300

    all done

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,5 @@
 const popupLinks = document.querySelectorAll('.popup-link');
 const body = document.querySelectorAll('.body');
-const lockPadding = document.querySelectorAll(".lock-padding");
-
-let unlock = true;
 
 const timeout = 800;
 
@@ -31,31 +28,38 @@ if (popupCloseIcon.length > 0) {
 
 
 function popupOpen(currentPopup) {
-    console.log(currentPopup);
         currentPopup.classList.add('open');
-        console.log(currentPopup);
         document.querySelector('.wrapper').classList.add('blur');
-
-        const popAct = document.querySelector('.popup.open'); 
 
         document.addEventListener('click', function(e){
             if (e.target.className !== 'popup-link') {
                 if (!e.target.closest('.popup')) {
-                    console.log('1');
                     popupClose(currentPopup);
                 }
-                e.preventDefault();
+                //e.preventDefault(); обновление страницы на крестик
             }
         });
        // popupClose(popAct);
 }
 
 
-
 function popupClose(popupActive) {
-    console.log(popupActive);
     popupActive.classList.remove('open');
     document.querySelector('.wrapper').classList.remove('blur');
-    console.log(popupActive);
 }
+
+///////////////////////////////////////// */
+//const PopupListener = document.querySelector('.header__chat');
+//const Popup = document.getElementById('popupRs');
+//const Bg = document.querySelector('.wrapper');
+//console.log(Bg);
+//console.log(PopupListener);
+//console.log(Popup);
+
+//PopupListener.addEventListener('click', function(e){
+//    Popup.classList.add('open');
+//    document.querySelector('.wrapper').classList.add('blur');
+//})  
+
+
 
